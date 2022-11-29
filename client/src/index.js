d376de6f3c4130fef75a9810f63bbb0fb9364f20
Himanshu <himanshu@ee.iitr.ac.in>
2022-11-29 17:04:29 +0530

    first commit

---
+++ client/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+// import { Provider } from 'react-redux'
+
+// import { configureStore, applyMiddleware, compose } from 'redux'
+
+import App from './App'
+
+// Render App to root element
+ReactDOM.render(<App />, document.getElementById('root'))
---
+++ client/src/styles.js
@@ -0,0 +1,38 @@
+import { makeStyles } from "@mui/styles"
+
+// Create styles
+export default makeStyles(() => ({
+	appBar: {
+		borderRadius: 15,
+		margin: '30px 0',
+		display: 'flex',
+		flexDirection: 'row',
+		justifyContent: 'center',
+		alignItems: 'center'
+	},
+	heading: {
+		color: 'rgba(0, 183, 255, 1)'
+	},
+	image: {
+		marginLeft: '15px'
+	}
+}))
+
+// export function useStyles () {
+// 	return ({
+// 		appBar: {
+// 			borderRadius: 15,
+// 			margin: '30px 0',
+// 			display: 'flex',
+// 			flexDirection: 'row',
+// 			justifyContent: 'center',
+// 			alignItems: 'center'
+// 		},
+// 		heading: {
+// 			color: 'rgba(0, 183, 255, 1)'
+// 		},
+// 		image: {
+// 			marginLeft: '15px'
+// 		}
+// 	})
+// }
