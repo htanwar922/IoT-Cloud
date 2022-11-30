39fe1eec70cdada06ec328358316e2df10969ba6
Himanshu <himanshu@ee.iitr.ac.in>
2022-11-30 15:27:12 +0530

    Added application list, background, appbar logo

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,33 @@
 import React from "react"
-import { Container, AppBar, Typography, Grow, Grid } from '@mui/material'
+import { Container, AppBar, Typography, Grow, Grid, Stack } from '@mui/material'
 
-// import Posts from "./components/Posts/Posts"
-// import Form from "./components/Form/Form"
-// import useStyles from './styles'
+import useStyles from './styles'
+import Applications from "./components/Applications/Applications"
 
-import memories from "./images/memories.png"
+// import bgImage from './images/IIT-Delhi.jpg'
+import logo from './images/IITD-Logo.jpg'
 
 // Create App
 function App() {
 	const classes = useStyles()
 	return (
-		<Container maxwidth="lg">
-			<AppBar className={classes.appBar} position="static" color="inherit">
-				<Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
-				<img className={classes.image} src={memories} alt="memories" height="60" />
+		<Container className={classes.appContainer} maxwidth="lg">
+			<AppBar className={classes.appBar} position="relative" color="inherit">
+				<Stack direction="row" alignItems="center" gap={1}>
+					<img className={classes.logo} src={logo} alt="memories" height="60" />
+					<Typography className={classes.heading} variant="h2"> <i>Baadal</i> - The IoT Cloud </Typography>
+					<img className={classes.logo} src={logo} alt="memories" height="60" />
+				</Stack>
 			</AppBar>
 			<Grow in>
 				<Container>
-					<Grid container justify="space-between" alignItems="stretch" spacing={3}>
-						<Grid item xs={12} sm={6}>
-							<Posts />
+					<Grid container alignItems="stretch" spacing={3}>
+						<Grid item>	{/*xs='auto' sm='auto'*/}
+							<Applications />
+							<Applications />
 						</Grid>
-						<Grid item xs={12} sm={6}>
-							<Form />
+						<Grid item> {/*xs={12} sm={6}*/}
+							<img className={classes.logo} src={logo} alt='IIT Delhi' height={400} />
 						</Grid>
 					</Grid>
 				</Container>
