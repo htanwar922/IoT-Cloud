260ace806f816c0adeb672c2465d866825c49b16
Himanshu <himanshu@ee.iitr.ac.in>
2022-11-30 20:47:29 +0530

    Image Background

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React from "react"
 import { Container, AppBar, Typography, Grow, Grid, Stack } from '@mui/material'
+import { ImageBackground } from 'react-native'
+// https://rn-master.com/applying-blur-effect-in-react-native/#:~:text=Applying%20Blur%20Effect%20In%20React%20Native%201%20Introduction,Blur%20In%20React%20Native%20...%205%20Conclusion%20
 
 import useStyles from './styles'
 import Applications from "./components/Applications/Applications"
@@ -11,28 +13,30 @@ import logo from './images/IITD-Logo.jpg'
 function App() {
 	const classes = useStyles()
 	return (
-		<Container className={classes.appContainer} maxwidth="lg">
-			<AppBar className={classes.appBar} position="relative" color="inherit">
-				<Stack direction="row" alignItems="center" gap={1}>
-					<img className={classes.logo} src={logo} alt="memories" height="60" />
-					<Typography className={classes.heading} variant="h2"> <i>Baadal</i> - The IoT Cloud </Typography>
-					<img className={classes.logo} src={logo} alt="memories" height="60" />
-				</Stack>
-			</AppBar>
-			<Grow in>
-				<Container>
-					<Grid container alignItems="stretch" spacing={3}>
-						<Grid item>	{/*xs='auto' sm='auto'*/}
-							<Applications />
-							<Applications />
+		<ImageBackground>
+			<Container className={classes.appContainer} maxwidth="lg">
+				<AppBar className={classes.appBar} position="relative" color="inherit">
+					<Stack direction="row" alignItems="center" gap={1}>
+						<img className={classes.logo} src={logo} alt="memories" height="60" />
+						<Typography className={classes.heading} variant="h2"> <i>Baadal</i> - The IoT Cloud </Typography>
+						<img className={classes.logo} src={logo} alt="memories" height="60" />
+					</Stack>
+				</AppBar>
+				<Grow in>
+					<Container>
+						<Grid container alignItems="stretch" spacing={3}>
+							<Grid item>	{/*xs='auto' sm='auto'*/}
+								<Applications />
+								{/* <Applications /> */}
+							</Grid>
+							<Grid item> {/*xs={12} sm={6}*/}
+								<img className={classes.logo} src={logo} alt='IIT Delhi' height={450} />
+							</Grid>
 						</Grid>
-						<Grid item> {/*xs={12} sm={6}*/}
-							<img className={classes.logo} src={logo} alt='IIT Delhi' height={400} />
-						</Grid>
-					</Grid>
-				</Container>
-			</Grow>
-		</Container>
+					</Container>
+				</Grow>
+			</Container>
+		</ImageBackground>
 	)
 }
 
